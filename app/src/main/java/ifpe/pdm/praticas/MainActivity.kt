11c2296c91feb9603cfb325cba19de6288ee9dcc
EduardoFreit <lepf@discente.ifpe.edu.br>
2023-03-28 16:13:57 -0300

    Parte 3: Tratando eventos

--- app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
+++ app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
@@ -1,11 +1,27 @@
 package ifpe.pdm.praticas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        val intent : Intent = Intent(this, DisplayMessageActivity::class)
+        val editText : EditText = findViewById<EditText>(R.id.edit_message)
+        val message : String = editText.text.toString();
+
+        intent.putExtra(EXTRA_MSG, message)
+
+        startActivity(intent);
+    }
+    companion object {
+        const val EXTRA_MSG = "Pratica01.MESSAGE"
     }
 }
