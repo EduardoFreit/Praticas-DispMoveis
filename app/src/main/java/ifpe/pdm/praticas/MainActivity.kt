a54f7235a43bfa8fa1cd855ad871bc2ec3b8692c
EduardoFreit <lepf@discente.ifpe.edu.br>
2023-03-28 16:50:14 -0300

    Parte 5: Trabalhando com a App Bar/Toolbar

--- app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
+++ app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
@@ -3,8 +3,12 @@ package ifpe.pdm.praticas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,5 +27,33 @@ class MainActivity : AppCompatActivity() {
     }
     companion object {
         const val EXTRA_MSG = "Pratica01.MESSAGE"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_search -> {
+                openSearch()
+                return true
+            }
+            R.id.action_settings -> {
+                openSettings()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun openSettings() {
+        Toast.makeText(this, "Não Implementada.", Toast.LENGTH_LONG).show()
+    }
+
+    private fun openSearch() {
+        Toast.makeText(this, "Não Implementada.", Toast.LENGTH_LONG).show()
     }
 }
