17ec1dbfbb3e93e0929f4d480059df684a1f3ae3
EduardoFreit <lepf@discente.ifpe.edu.br>
2023-04-04 18:34:57 -0300

    Parte 2: Criando um layout para os itens da lista.

--- app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
+++ app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
@@ -1,9 +1,14 @@
 package ifpe.pdm.praticas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +23,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val listView : ListView = findViewById(R.id.list_view)
-        listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, CITIES)
+        listView.adapter = ArrayAdapter(
+            this,
+            R.layout.city_listitem,
+            R.id.city_name,
+            CITIES
+        )
+
+        listView.onItemClickListener =
+            OnItemClickListener { parent: AdapterView<*>, view: View?, position: Int, id: Long ->
+                Toast.makeText(
+                    parent.context,
+                    "Cidade selecionada: " + CITIES[position],
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
     }
 }
