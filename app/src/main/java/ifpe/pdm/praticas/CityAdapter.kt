b27e699de4eb8fe9e8dbd9a6f7b0c4696c9fdb62
EduardoFreit <lepf@discente.ifpe.edu.br>
2023-04-04 19:51:25 -0300

    Parte 4: Melhorando o desempenho do ListView com Reciclagem

--- app/src/main/java/ifpe/pdm/praticas/CityAdapter.kt
+++ app/src/main/java/ifpe/pdm/praticas/CityAdapter.kt
@@ -11,10 +11,15 @@ import android.widget.TextView
 class CityAdapter(context: Context, resource: Int, private val cities: Array<City>) : ArrayAdapter<City>(context, resource, cities) {
 
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
-        val inflater: LayoutInflater = LayoutInflater.from(context)
-        val listItem: View = inflater.inflate(R.layout.city_listitem, null, true)
-        val cityName: TextView = listItem.findViewById(R.id.city_name)
-        val cityInfo: TextView = listItem.findViewById(R.id.city_info)
+        var listItem: View? = view
+
+        if (listItem == null) {
+            val inflater: LayoutInflater = LayoutInflater.from(context)
+            listItem = inflater.inflate(R.layout.city_listitem, parent, false)
+        }
+
+        val cityName: TextView = listItem!!.findViewById(R.id.city_name)
+        val cityInfo: TextView = listItem!!.findViewById(R.id.city_info)
         cityName.text = cities[position].name
         cityInfo.text = cities[position].info
         return listItem
