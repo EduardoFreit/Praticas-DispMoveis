bd471fe4fb8605522120f7e49202bb785423076e
EduardoFreit <lepf@discente.ifpe.edu.br>
2023-04-04 18:15:29 -0300

    Parte 1: Criando a atividade principal

--- app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
+++ app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
@@ -2,10 +2,22 @@ package ifpe.pdm.praticas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val CITIES : Array<String> = arrayOf("Recife", "João Pessoa", "Natal",
+            "Fortaleza", "Rio de Janeiro", "São Paulo", "Salvador", "Vitória",
+            "Florianópolis", "Porto Alegre", "São Luiz","Teresina",
+            "Belém", "Manaus")
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView : ListView = findViewById(R.id.list_view)
+        listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, CITIES)
     }
 }
