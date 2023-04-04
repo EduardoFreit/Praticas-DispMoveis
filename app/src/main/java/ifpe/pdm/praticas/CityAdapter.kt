2cad3e7bd19fccb7917333474ee73fbe709b00c7
EduardoFreit <lepf@discente.ifpe.edu.br>
2023-04-04 20:08:17 -0300

    Parte 5: Melhorando o desempenho do ListView com ViewHolder

--- app/src/main/java/ifpe/pdm/praticas/CityAdapter.kt
+++ app/src/main/java/ifpe/pdm/praticas/CityAdapter.kt
@@ -12,16 +12,24 @@ class CityAdapter(context: Context, resource: Int, private val cities: Array<Cit
 
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         var listItem: View? = view
+        var holder: ViewHolder?
 
         if (listItem == null) {
             val inflater: LayoutInflater = LayoutInflater.from(context)
             listItem = inflater.inflate(R.layout.city_listitem, parent, false)
+
+            holder = ViewHolder()
+            holder.cityName = listItem!!.findViewById((R.id.city_name))
+            holder.cityInfo = listItem!!.findViewById((R.id.city_info))
+
+            listItem.tag = holder
+        } else {
+            holder = view?.tag as ViewHolder
         }
 
-        val cityName: TextView = listItem!!.findViewById(R.id.city_name)
-        val cityInfo: TextView = listItem!!.findViewById(R.id.city_info)
-        cityName.text = cities[position].name
-        cityInfo.text = cities[position].info
+        holder.cityName.text = cities[position].name
+        holder.cityInfo.text = cities[position].info
+
         return listItem
     }
 }
---
+++ app/src/main/java/ifpe/pdm/praticas/ViewHolder.kt
@@ -0,0 +1,8 @@
+package ifpe.pdm.praticas
+
+import android.widget.TextView
+
+class ViewHolder {
+    lateinit var cityName: TextView
+    lateinit var cityInfo: TextView
+}
