20203b659d6f9eacf65c9868a416f3aff75223ec
EduardoFreit <lepf@discente.ifpe.edu.br>
2023-04-29 12:25:42 -0300

    Parte 3: Registrando o usuário na base

--- app/src/main/java/ifpe/pdm/praticas/SignUpActivity.kt
+++ app/src/main/java/ifpe/pdm/praticas/SignUpActivity.kt
@@ -7,7 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import ifpe.pdm.praticas.databinding.ActivitySignUpBinding
+import ifpe.pdm.praticas.model.User
 
 
 class SignUpActivity : AppCompatActivity() {
@@ -35,6 +38,7 @@ class SignUpActivity : AppCompatActivity() {
         fbAuth.removeAuthStateListener(authListener)
     }
     private fun buttonSignUpClick(view: View) {
+        val name: String = binding.editName.text.toString()
         val email: String = binding.editEmail.text.toString()
         val password: String = binding.editPassword.text.toString()
         val mAuth = FirebaseAuth.getInstance()
@@ -45,6 +49,12 @@ class SignUpActivity : AppCompatActivity() {
                     this@SignUpActivity, msg,
                     Toast.LENGTH_SHORT
                 ).show()
+
+                if(task.isSuccessful) {
+                    var tempUser = User(name, email)
+                    val drUsers: DatabaseReference = FirebaseDatabase.getInstance().getReference("users")
+                    mAuth.currentUser?.let { drUsers.child(it.uid).setValue(tempUser) }
+                }
             }
     }
 }
