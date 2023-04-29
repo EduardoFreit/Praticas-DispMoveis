06b457b8c0add21a684a2bf6fb29ba6929e18eb8
EduardoFreit <lepf@discente.ifpe.edu.br>
2023-04-29 14:10:50 -0300

    Parte 4: Preparando a interface do Chat

--- app/src/main/java/ifpe/pdm/praticas/HomeActivity.kt
+++ app/src/main/java/ifpe/pdm/praticas/HomeActivity.kt
@@ -1,11 +1,14 @@
 package ifpe.pdm.praticas
 
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 import ifpe.pdm.praticas.databinding.ActivityHomeBinding
+import ifpe.pdm.praticas.model.Message
 
 
 class HomeActivity : AppCompatActivity() {
@@ -24,6 +27,13 @@ class HomeActivity : AppCompatActivity() {
 
         this.fbAuth = FirebaseAuth.getInstance()
         this.authListener = FirebaseAuthListener(this)
+
+        binding.sendMessage.setOnClickListener {
+            val userName = "Test"
+            val message: String? = binding.editMessage.text.toString()
+            val newMessage = Message(userName, message)
+            this.showMessage(newMessage)
+        }
     }
     override fun onStart() {
         super.onStart()
@@ -32,6 +42,12 @@ class HomeActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         fbAuth.removeAuthStateListener(authListener)
+    }
+
+    private fun showMessage(message: Message) {
+        val tvMsg = TextView(this)
+        tvMsg.text = message.name + ": " + message.text
+        binding.chatArea.addView(tvMsg)
     }
     private fun sair() {
         val mAuth = FirebaseAuth.getInstance()
