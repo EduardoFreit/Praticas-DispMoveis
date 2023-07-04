8e72aec51c364f8e11071f6820dcb0ef8f048848
lepf <lepf@discente.ifpe.edu.br>
2023-07-04 18:52:09 -0300

    Parte 2: Recebendo o alarme e gerando uma notificação para o usuário.

---
+++ app/src/main/java/ifpe/pdm/praticas/AlarmReceiver.kt
@@ -0,0 +1,51 @@
+package ifpe.pdm.praticas
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+class AlarmReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if(context != null) {
+            val newIntent = Intent(context, MainActivity::class.java)
+            newIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            val pendingNotificationIntent = PendingIntent.getActivity(
+                context, 0, newIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val channelId = createChannel(context)
+
+            val builder = NotificationCompat.Builder(context, channelId!!)
+            builder.setSmallIcon(R.mipmap.ic_launcher)
+            builder.setContentText("Alarme disparou! Toque para reagendar.")
+            builder.setContentIntent(pendingNotificationIntent)
+            builder.setAutoCancel(true)
+            val notification = builder.build()
+
+            val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(0, notification)
+        }
+    }
+
+    private fun createChannel(context: Context): String? {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "Pratica11"
+            val channel = NotificationChannel(
+                channelId,
+                "Canal Pratica11",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+            return channelId
+        } else {
+            return null
+        }
+    }
+}
--- app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
+++ app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
@@ -1,7 +1,12 @@
 package ifpe.pdm.praticas
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ifpe.pdm.praticas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +17,25 @@ class MainActivity : AppCompatActivity() {
 
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        viewBinding.buttonAgendar.setOnClickListener { scheduleAlarm() }
+    }
+
+    private fun scheduleAlarm() {
+        val time: Long = System.currentTimeMillis() + 10 * 1000
+
+        val intentAlarm = Intent(this, AlarmReceiver::class.java)
+
+        val pendingAlarmIntent = PendingIntent.getBroadcast(
+            this, 1, intentAlarm, PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP, time, pendingAlarmIntent)
+
+        Toast.makeText(this, "Alarme agendado.", Toast.LENGTH_LONG).show()
+
+        finish()
     }
 }
