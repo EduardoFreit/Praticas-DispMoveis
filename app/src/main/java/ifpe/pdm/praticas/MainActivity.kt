68950b66f78f1bbc01840330a882993a73c84422
lepf <lepf@discente.ifpe.edu.br>
2023-07-04 18:04:49 -0300

    Parte 1: Criando a atividade principal

--- app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
+++ app/src/main/java/ifpe/pdm/praticas/MainActivity.kt
@@ -2,10 +2,15 @@ package ifpe.pdm.praticas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ifpe.pdm.praticas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
     }
 }
